1f2d7b2b91d5b93d0abc638b470f9ecf4b20b2ca
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-10-31 16:39:48 -0700

    replaces var with const for increased code clarity

--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,9 @@
 import config from "./config.js"
 
-var FluxSdk = window.FluxSdk
-var FluxHelpers = window.FluxHelpers
+const FluxSdk = window.FluxSdk
+const FluxHelpers = window.FluxHelpers
 
-var sdk = new FluxSdk(config.flux_client_id, { redirectUri: config.url, fluxUrl: config.flux_url })
-var helpers = new FluxHelpers(sdk)
+const sdk = new FluxSdk(config.flux_client_id, { redirectUri: config.url, fluxUrl: config.flux_url })
+const helpers = new FluxHelpers(sdk)
 
 export default helpers
