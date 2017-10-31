cfc4f694598c14f966b14ac169a9e28380d25abf
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-10-30 20:30:52 -0700

    includes matching client id

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     url: 'http://localhost:8080', // your url
     flux_url: 'https://flux.io', // flux url
-    flux_client_id: '', // your app's client id
+    flux_client_id: '74e63a6f-956a-44a5-ad33-d8245d28696a', // your app's client id
   }
